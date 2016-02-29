48283ed277d2b69a948113d664a22209e72846ce
rhanberry <rhanberry@gmail.com>
2016-02-29 12:25:02 -0800

    this should work fine now

--- target/classes/scripts/app/main/main.controller.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('expressoApp')
-    .controller('MainController', function ($scope, $state) {
-        $scope.$state = $state;
-
-
-    });
--- target/classes/scripts/components/navbar/navbar.controller.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-angular.module('expressoApp')
-    .controller('NavbarController', function ($scope, $location, $state) {
-        $scope.$state = $state;
-
-       });
